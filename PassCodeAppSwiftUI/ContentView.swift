4e0aeb12869af5970c2187fc853ee5c2a2c8f118
CARD1NAL <cardinal312@github.com>
2024-04-23 07:25:46 +0600

    Initial Commit

---
+++ PassCodeAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PassCodeAppSwiftUI
+//
+//  Created by Macbook on 23/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PassCodeAppSwiftUI/PassCodeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassCodeAppSwiftUIApp.swift
+//  PassCodeAppSwiftUI
+//
+//  Created by Macbook on 23/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PassCodeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
